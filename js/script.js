495d0f16b609778a08583adf88bae596fe3d0356
khizhniakov <91787074+khizhniakov@users.noreply.github.com>
2022-09-08 12:37:47 +0300

    update

--- js/script.js
+++ js/script.js
@@ -5,54 +5,11 @@ menuBtn.onclick = () => {
   menuBtn.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
-
 window.onscroll = () => {
   menuBtn.classList.remove("fa-times");
   navbar.classList.remove("active");
 };
-
-var swiper = new Swiper(".course-slider", {
-  spaceBetween: 20,
-  grabCursor: true,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-var swiper = new Swiper(".teachers", {
-  spaceBetween: 20,
-  grabCursor: true,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
-
+// slider
 var swiper = new Swiper(".reviews-slider", {
   spaceBetween: 20,
   grabCursor: true,
@@ -73,3 +30,43 @@ var swiper = new Swiper(".reviews-slider", {
     },
   },
 });
+
+//animation
+const animItems = document.querySelectorAll("._anim-items");
+
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll(params) {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_active");
+      } else {
+        if (!animItem.classList.contains("_anim-no-hide")) {
+          animItem.classList.remove("_active");
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+  setTimeout(() => {
+    animOnScroll();
+  }, 300);
+}
